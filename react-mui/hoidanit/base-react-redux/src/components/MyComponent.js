86b24e08a65d6b6b665007652ed00ec7d15cabfc
lcaohoanq <hoangclw@gmail.com>
2024-10-06 10:39:24 +0700

    add state

--- react-mui/hoidanit/base-react-redux/src/components/MyComponent.js
+++ react-mui/hoidanit/base-react-redux/src/components/MyComponent.js
@@ -4,12 +4,27 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  //state la trang thai
+  //state la object
+  //state la mot cach ma react kiem soat data cua component
+  //khi state thay doi, component render lai
+  state = {
+    name: "Hieu",
+    address: "Ha Noi",
+    age: 18,
+  };
+
   //JSX
   render() {
     return (
       <div>
         My First Component
         {Math.random()}
+        <div>
+          <div>Name: {this.state.name}</div>
+          <div>Address: {this.state.address}</div>
+          <div>Age: {this.state.age}</div>
+        </div>
       </div>
     );
   }
